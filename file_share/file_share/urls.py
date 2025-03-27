d21cb1665e776b7d2fe7a4eeb5fc596583f51c64
shashankssp12 <shashankspandey12@gmail.com>
2025-03-27 20:44:35 +0530

    Add template views and update URL routing for user authentication and as per template views

--- file_share/file_share/urls.py
+++ file_share/file_share/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('share.urls')),
+    path('api/', include('share.urls')),  # For API views
+    path('', include('share.urls')),  # For template views
 ]
